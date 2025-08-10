767a32f8c6f705492d9af5cb7574c31b5fd33f6f
Trần Mạnh Hưng <tranhung26122612@gmail.com>
2025-08-10 18:27:56 +0700

    feat: login user

--- src/queries/users.query.ts
+++ src/queries/users.query.ts
@@ -1,10 +1,15 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { QUERY_CURRENT_USER_DETAILS_KEY } from '@/constants/query.constant';
-import { getCurrentUserDetails } from '@/repositories/users/userRepositories';
+import { getCurrentUserDetails, loginUser } from '@/repositories/users/userRepositories';
 
 export const useCurrentUserDetails = (token: string | null) =>
   useQuery({
     queryFn: () => getCurrentUserDetails().then((res) => res.data),
     queryKey: [QUERY_CURRENT_USER_DETAILS_KEY, token],
     enabled: !!token,
+  });
+
+export const useLoginUserMutation = () =>
+  useMutation({
+    mutationFn: loginUser,
   });
